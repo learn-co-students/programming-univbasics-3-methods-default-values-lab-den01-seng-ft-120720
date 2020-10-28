7e0016e0e8f5e40435594203f61563eb5aadb384
Jack Hubert <jphubert93@gmail.com>
2020-10-28 20:23:36 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -5,5 +5,5 @@ def meal_choice(veg1, veg2, protein="tofu")
   puts "What a nutritious meal!"
   meal_statement = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal_statement
-  return meal_statement
+  meal_statement
 end
